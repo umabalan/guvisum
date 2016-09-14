3dac7f458ac501ec2072cbe710b591042fb910d3
umabalan <mailuma2910@gmail.com>
2016-09-14 10:22:09 +0530

    Update nodigit.java

--- nodigit.java
+++ nodigit.java
@@ -7,7 +7,7 @@ public static void main(String[] args)
 Scanner sc=new Scanner(System.in);
 int n=sc.nextInt();
 String s=String.valueOf(n);
-int n=s.length();
+n=s.length();
 System.out.println(n);
 }
 }

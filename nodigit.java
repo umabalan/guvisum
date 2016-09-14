ff8e13d68101dfdf19615bba6628645bac6f6f6a
umabalan <mailuma2910@gmail.com>
2016-09-14 10:20:48 +0530

    Create nodigit.java

---
+++ nodigit.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Noofdigits
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+String s=String.valueOf(n);
+int n=s.length();
+System.out.println(n);
+}
+}
